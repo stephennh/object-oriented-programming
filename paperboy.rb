ab3ba178548308cd208e29144f09396b28dba52c
Stephen H <stephen210huang@hotmail.com>
2017-01-18 15:46:34 -0500

    paperboy.rb base structure done

--- paperboy.rb
+++ paperboy.rb
@@ -1,6 +1,27 @@
 class Paperboy
 
+  attr_reader :name, :experience, :earnings
+
   def initialize(name, experience, earnings)
+    @name = name
+    @experience = experience
+    @earnings = earnings
+  end
+
+
+  def quota
+
+  end
+
+
+  def deliver(start_address, end_address)
+
+  end
+
+
+  def report
+    puts "I'm #{name}, I've delivered #{experience} and I've earned #{earnings} so far!"
+  end
 
 
 end
