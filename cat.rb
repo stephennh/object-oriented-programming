801d5e2a0bcc74fb89edfe406b770a14443c9b65
Stephen H <stephen210huang@hotmail.com>
2017-01-18 14:23:27 -0500

    cat.rb complete

--- cat.rb
+++ cat.rb
@@ -8,18 +8,19 @@ class Cat
   end
 
 # 4
-  def eats_at(meal_time)
-    if
-      meal_time <= 12
+  def eats_at
+    if @meal_time <= 11
       "#{ @meal_time }AM"
-    else
+    elsif @meal_time == 12
       "#{ @meal_time }PM"
+    else
+      "#{ @meal_time - 12}PM"
     end
   end
 
 # 5
   def meow
-  "My name is #{ @name } and I eat #{ @preferred_food } at #{ meal_time }"
+  "My name is #{ @name } and I eat #{ @preferred_food } at #{ eats_at }"
   end
 
 
--- player.rb
+++ player.rb
@@ -7,6 +7,7 @@ class Player
     @health_points == 10
   end
 
+# 6
   def do_battle
     1 - @health_points
     if @health_points == 0
@@ -19,6 +20,29 @@ class Player
   end
 
 
+# 7
+  def restart
+    @lives == 5
+    @gold_coins == 0
+    @score == 0
+    @health_points == 10
+  end
 
+
+# 8
+  def level_up
+    @lives += 1
+  end
+
+# 9
+  def collect_treasure(treasure)
+    @gold_coins + treasure
+    if @gold_coins == 10
+      @score += 1
+      if @score == 10
+        level_up
+      end
+    end
+  end
 
 end
