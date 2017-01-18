3e06f3fd9add340ed957890f26f957f067eb0095
Stephen H <stephen210huang@hotmail.com>
2017-01-18 16:23:55 -0500

    paperboy.rb quota and deliver method formed

--- paperboy.rb
+++ paperboy.rb
@@ -4,18 +4,33 @@ class Paperboy
 
   def initialize(name, experience, earnings)
     @name = name
-    @experience = experience
+    # the number of papers they've delivered
+    @experience = 50
+    # amount of money they've earned
     @earnings = earnings
   end
 
 
-  def quota
-
+  def quota(paper_del)
+    @quota = @quota + (paper_del / 2)
   end
 
 
   def deliver(start_address, end_address)
-
+    # seeing how much was delivered
+    total_del = end_address - start_address
+    # if the total delivered was over the quota
+    if total_del > quota
+      # take the excess and multiply by 0.5 for $0.50 per each over quota
+      ovr_quot = total_del - quota * 0.5
+      # quota delivered multiply by 0.25 for $0.25 per each in quota
+      udr_quot = quota * 0.25
+      #total earning is the quota($0.25) + over quota($0.5)
+      total_earn = udr_quot + ovr_quot
+    else
+      # total earned is the total delivered multiply by value of $0.25
+      total_earn = total_del * 0.25
+    end
   end
 
 
